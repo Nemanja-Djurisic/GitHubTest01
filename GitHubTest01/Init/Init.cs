1659ad41d905d7c0262ade0a67e71303136b12ee
nemanja.djurisic <nemanja.djurisic.dev@gmail.com>
2016-03-28 15:51:04 +0200

    added Init class

---
+++ GitHubTest01/Init/Init.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using GitHubTest01.Pages;
+using GitHubTest01.Init;
+
+namespace GitHubTest01.Init
+{
+    class Init
+    {
+        public void GoToHomePage(IWebDriver driver)
+        {
+            HomePage _homePage = new HomePage(driver);
+
+            _homePage.GoTo();
+            _homePage.MaxWin();
+        }
+
+        public void FinishTest(IWebDriver driver)
+        {
+            HomePage _homePage = new HomePage(driver);
+
+            driver.Quit();
+        }
+    }
+}
