4eaaa5a5abe010bf0cc5bcbede6ddb1ec170990c
nemanja.djurisic <nemanja.djurisic.dev@gmail.com>
2016-03-28 14:21:03 +0200

    new pages

---
+++ GitHubTest01/Pages/HomePageTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GW_POP_Test01.Pages.UsersAndExtensions
+{
+    public class PageName
+    {
+        public readonly IWebDriver driver;
+
+        public PageName(IWebDriver browser)
+        {
+            this.driver = browser;
+            PageFactory.InitElements(browser, this);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(20));
+        }
+
+        //Element - Field
+        [FindsBy(How = How.ClassName, Using = "location")]
+        public IWebElement ElementName { get; set; }
+
+        public void Action()
+        {
+
+        }
+    }
+}
